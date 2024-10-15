0c56233ae2f3162927ab13e7905f8e1f7570a2b3
marino222 <marino@zwyssig.me>
2024-10-15 21:08:37 +0200

    update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Markup: Text Readability Test",
+        "excerpt":"Portland in shoreditch Vice, labore typewriter pariatur hoodie fap sartorial Austin. Pinterest literally occupy Schlitz forage. Odio ad blue bottle vinyl, 90’s narwhal commodo bitters pour-over nostrud. Ugh est hashtag in, fingerstache adipisicing laboris esse Pinterest shabby chic Portland. Shoreditch bicycle rights anim, flexitarian laboris put a bird on it...","categories": [],
+        "tags": ["sample post","readability","test"],
+        "url": "/markup-text-readability/",
+        "teaser": null
+      }]
